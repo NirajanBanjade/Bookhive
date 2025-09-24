e6e4121078cf5b0258302432151115d01f34f393
Prashant Panta <uyw13@txstate.edu>
2025-09-24 01:15:03 -0500

    Fresh Branch

--- Backend/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/profile", methods=["GET", "POST"])
-def profile():
-    # TODO: handle form submit later
-    return render_template("profile.html")
