3d4df516887b3cb57d8d4cba503abe5166802474
Alan Garcia <garciaalan0421@gmail.com>
2025-09-30 17:17:42 -0500

    Added routes for pages in website so far

--- bookhive/frontend/src/App.js
+++ bookhive/frontend/src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import SearchPage from "./pages/searchPage";
 import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <Profile />
-    </div>
+    <Router>
+
+      <div className="App">
+
+        {/* <Profile /> */}
+        <nav style={{marginBottom: "1rem"}}>
+          <Link to="/profile" style={{ marginRight: "1rem" }}>Profile</Link>
+          <Link to="/search">Search Books</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/search" element={<SearchPage />} />
+        </Routes>
+
+      </div>
+
+    </Router>
   );
 }
 
