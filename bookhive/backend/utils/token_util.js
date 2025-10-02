2d93869e47f0bd67933c3133c055f7d1d6658bbe
Nirajan Banjade <nirajanbanjade321@gmail.com>
2025-10-02 09:09:36 -0600

    KAN-27 saving password history not including currrent one.

--- bookhive/backend/utils/token_util.js
+++ bookhive/backend/utils/token_util.js
@@ -3,7 +3,8 @@ const crypto = require('crypto');
 async function issueResetCode(userDoc) { 
   const code = String(crypto.randomInt(0, 1_000_000)).padStart(6, '0');
   userDoc.resetOtpPlain = code;
-  userDoc.resetOtpExpiresAt = new Date(Date.now() + 10 * 60 * 1000);
+//   userDoc.resetOtpExpiresAt = new Date(Date.now() + 10 * 60 * 1000);
+  userDoc.resetOtpExpiresAt = new Date(Date.now());
   await userDoc.save();
   return code;
 }
