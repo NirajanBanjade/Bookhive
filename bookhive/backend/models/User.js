710cc8f215ef63844f9e611ce8b04d8a0dea284c
Nirajan Banjade <nirajanbanjade321@gmail.com>
2025-10-01 18:51:42 -0600

    KAN-27 created a list data structure to store n(currently 3) password hashes, to prevent reuse of passwords

--- bookhive/backend/models/User.js
+++ bookhive/backend/models/User.js
@@ -40,7 +40,20 @@ const User = new mongoose.Schema(
 User.methods.setPassword = async function (plain) {
 
     const rounds = parseInt(process.env.BCRYPT_SALT_ROUNDS || '12', 10);
-    this.passwordHash = await bcrypt.hash(plain, rounds);
+    // this.passwordHash = await bcrypt.hash(plain, rounds);
+    const passwordHash=await bcrypt.hash(plain, rounds);
+    
+    for(let pass in this.passwordHistory){
+        const match= await bcrypt.compare(plain, this.passwordHistory[pass]);
+        if(match) throw new Error('New password must be different from last three passwords!');
+    }
+    this.passwordHash = passwordHash;
+    this.passwordHistory.unshift(passwordHash); // added the new password hash to the start of the array.
+
+    if(this.passwordHistory.length>3){
+        this.passwordHistory.pop(); // keep only last three passwords.
+    }
+
 };
 User.methods.verifyPassword = function (plain) {
     return bcrypt.compare(plain, this.passwordHash);
