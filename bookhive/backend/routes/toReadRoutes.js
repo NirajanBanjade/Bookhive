44156d9ef039517e297caa0945b325930e997651
lav115 <lav115@txstate.edu>
2025-10-02 17:24:08 -0500

    KAN-13 remove redundant search route in toReadRoutes for tracking

--- bookhive/backend/routes/toReadRoutes.js
+++ bookhive/backend/routes/toReadRoutes.js
@@ -11,34 +11,20 @@ const {
   getNotifications
 } = require('../controllers/toReadController');
 
-// --------------------
-// GET routes
-// --------------------
 
-// Get a user's to-read list
+// get routes
 router.get('/:userId', getToReadList);
 
-// Search books in to-read list
 router.get('/:userId/search', searchToReadBooks);
 
-// Get notifications for a user
 router.get('/:userId/notifications', getNotifications);
 
-// --------------------
-// POST routes
-// --------------------
-
-// Add a book to to-read
+// post routes
 router.post('/:userId', addBookToToRead);
 
-// Move a book from to-read to collections
 router.post('/:userId/:googleBookId/move-to-collections', moveBookToCollections);
 
-// --------------------
-// DELETE routes
-// --------------------
-
-// Remove a book from to-read list
+// delete routes
 router.delete('/:userId/:googleBookId', removeBookFromToRead);
 
 module.exports = router;
