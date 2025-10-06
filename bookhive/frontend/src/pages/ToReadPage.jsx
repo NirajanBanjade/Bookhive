01a03395e662f2f7e9b85c8a650477da8d45bda9
Nirajan Banjade <lra84@txstate.edu>
2025-10-06 00:14:35 -0600

    port change from 5050 to 5000

--- bookhive/frontend/src/pages/ToReadPage.jsx
+++ bookhive/frontend/src/pages/ToReadPage.jsx
@@ -16,7 +16,7 @@ const ToReadPage = () => {
 
     try {
       const response = await axios.post(
-        `http://localhost:5000/api/to-read/${userId}`,
+        `http://localhost:5050/api/to-read/${userId}`,
         demoBook // send fields directly, NOT wrapped in 'book'
       );
       console.log('Book added:', response.data.books || response.data.list?.books);
@@ -31,7 +31,7 @@ const ToReadPage = () => {
   const handleRemoveBook = async (bookId) => {
     try {
       const response = await axios.delete(
-        `http://localhost:5000/api/to-read/${userId}/${bookId}`
+        `http://localhost:5050/api/to-read/${userId}/${bookId}`
       );
       setBooks(response.data.list.books);
       alert('Book removed from your To-Read list');
