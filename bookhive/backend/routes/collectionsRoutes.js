9ebd3d6d5ba7021a93410a318a57c28d3bbc14b3
lav115 <lav115@txstate.edu>
2025-10-15 11:12:41 -0500

    KAN-59: Finalize status support and comment future endpoints

--- bookhive/backend/routes/collectionsRoutes.js
+++ bookhive/backend/routes/collectionsRoutes.js
@@ -4,5 +4,8 @@ const collectionsController = require('../controllers/collectionsController');
 
 router.get('/:userId', collectionsController.getCollectionsList);
 router.post('/:userId', collectionsController.addBookToCollections);
+router.put('/:userId/books/:googleBookId/status', collectionsController.updateBookStatus);
+// router.put('/:userId/books/:googleBookId/review', collectionsController.addReview); // KAN-60: Future review support
+// router.get('/:userId/stats', collectionsController.getProfileStats); // KAN-60: Future profile stats
 
 module.exports = router;
