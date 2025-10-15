d8f3430fe872616423f7e2378fd610cbb3394c31
lav115 <lav115@txstate.edu>
2025-10-15 10:12:20 -0500

    KAN-59: Mount Collections routes in app.js

--- bookhive/backend/app.js
+++ bookhive/backend/app.js
@@ -68,3 +68,6 @@ module.exports = app;
 // Profile API routes - handles user profile viewing and editing
 const profileRoutes = require('./routes/profileRoutes');
 app.use('/api/profile', profileRoutes);
+
+const collectionsRoutes = require('./routes/collectionsRoutes');  
+app.use('/api/collections', collectionsRoutes);
