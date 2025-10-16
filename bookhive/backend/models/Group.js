8092b7c0bb5874132c02b1225c0ebab51048c0bf
Nirajan Banjade <lra84@txstate.edu>
2025-10-16 08:20:50 -0500

    KAN-58 Schema modified for each category group rather than each book group.

--- bookhive/backend/models/Group.js
+++ bookhive/backend/models/Group.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const BookGroupSchema = new mongoose.Schema(
     {
-        bookId: { type: String, required: true },
+        categorykey: { type: String, required: true },
         slug: { type: String, required: true }, // for URL slugs.
         name: { type: String, required: true },
         membersCount: { type: Number, default: 0, min: 0 },
-        createdBy:   { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        createdBy:   { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     },
     { timestamps: true }
 );
