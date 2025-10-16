6c3a355222552a12ae7a7290b6e4da7b3613583f
Nirajan Banjade <lra84@txstate.edu>
2025-10-16 08:55:18 -0500

    KAN-58 category based group creation, avoiding duplicates, and trimmed group names.

--- bookhive/backend/models/Group.js
+++ bookhive/backend/models/Group.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
+
+function toKey(s) { // normalization of category names.....
+    return s?.toString()
+      .normalize('NFKD')               // split accents
+      .replace(/[\u0300-\u036f]/g, '') // remove accents
+      .trim()
+      .toLowerCase()
+      .replace(/&/g, 'and')
+      .replace(/[^a-z0-9]+/g, '-')     // collapse to hyphens
+      .replace(/(^-|-$)/g, '');        // trim hyphens
+  }
+
 const BookGroupSchema = new mongoose.Schema(
     {
-        categorykey: { type: String, required: true },
-        slug: { type: String, required: true }, // for URL slugs.
+        categoryKey: { type: String, required: true, trim: true, set:toKey, unique: true, index: true},
         name: { type: String, required: true },
         membersCount: { type: Number, default: 0, min: 0 },
         createdBy:   { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
@@ -11,7 +22,6 @@ const BookGroupSchema = new mongoose.Schema(
     { timestamps: true }
 );
 
-BookGroupSchema.index({ bookId: 1 }, { unique: true });
-BookGroupSchema.index({ slug: 1 },   { unique: true });
+BookGroupSchema.index({ categoryKey: 1 }, { unique: true });
 
 module.exports = mongoose.model('BookGroup', BookGroupSchema);
