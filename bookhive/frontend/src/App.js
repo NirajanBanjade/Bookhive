625551919577bcc447c84cf8efcba9cde51d8159
Prashant Panta <uyw13@txstate.edu>
2025-10-17 18:15:39 -0500

    KAN-76 add dynamic genre/:genreId route for filtered book results

--- bookhive/frontend/src/App.js
+++ bookhive/frontend/src/App.js
@@ -7,22 +7,34 @@ import ToReadPage from "./pages/ToReadPage";
 import Loginpage from "./components/Loginpage/Loginpage";
 import HomePage from "./components/Homepage/HomePage";
 import GenrePage from "./pages/GenrePage";
+import GenreResultsPage from "./pages/GenreResultsPage";
 
 /**
- * Main App Component
+ * Main App Component - Application Routing
  *
- * Routes:
+ * Current Routes:
  * - / : User profile
- * - /home : Home feed with activity
- * - /search : Book search
+ * - /home : Home feed with friend activity
+ * - /search : Book search (title, author, keywords)
  * - /to-read : User's to-read list
- * - /genre : Browse all genres (NEW)
- * - /login : Authentication
+ * - /genre : Browse all genres (grid view)
+ * - /genre/:genreId : Filtered books by genre (NEW)
+ * - /login : Authentication page
  *
- * TODO for team:
- * - Add /genre/:subject route in next phase for filtered results
- * - Add protected routes when auth is implemented
- * - Consider adding 404 page
+ * TODO Phase 2 - Future Routes to Add:
+ * - /book/:googleBookId : Individual book detail page
+ * - /author/:authorName : Author's books page
+ * - /communities : Reddit-style book communities
+ * - /community/:communityId : Single community view
+ * - /user/:userId : Other user's public profile
+ * - /settings : User settings and preferences
+ * - /notifications : User notifications center
+ *
+ * TODO Phase 3 - Protected Routes:
+ * When authentication is complete, wrap routes with ProtectedRoute component:
+ * <Route path="/to-read" element={<ProtectedRoute><ToReadPage /></ProtectedRoute>} />
+ *
+ * @component
  */
 function App() {
   return (
@@ -32,12 +44,27 @@ function App() {
 
         <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
           <Routes>
+            {/* Authentication */}
             <Route path="/login" element={<Loginpage />} />
+
+            {/* Main Pages */}
             <Route path="/" element={<Profile />} />
             <Route path="/home" element={<HomePage />} />
             <Route path="/search" element={<SearchPage />} />
             <Route path="/to-read" element={<ToReadPage />} />
+
+            {/* Genre Routes */}
             <Route path="/genre" element={<GenrePage />} />
+            <Route path="/genre/:genreId" element={<GenreResultsPage />} />
+
+            {/* TODO Phase 2: Add these routes when pages are ready */}
+            {/* <Route path="/book/:googleBookId" element={<BookDetailPage />} /> */}
+            {/* <Route path="/author/:authorName" element={<AuthorPage />} /> */}
+            {/* <Route path="/communities" element={<CommunitiesPage />} /> */}
+            {/* <Route path="/community/:communityId" element={<CommunityPage />} /> */}
+            {/* <Route path="/user/:userId" element={<UserProfilePage />} /> */}
+            {/* <Route path="/settings" element={<SettingsPage />} /> */}
+            {/* <Route path="*" element={<NotFoundPage />} /> */}
           </Routes>
         </main>
       </div>
