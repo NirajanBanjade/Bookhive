e49ff930271fd28d927e9869bf2db746e7d1f4cb
Prashant Panta <uyw13@txstate.edu>
2025-10-17 17:31:54 -0500

    KAN-76 created a genre.js filder under src/components and listed todo

---
+++ bookhive/frontend/src/constants/genre.js
@@ -0,0 +1,13 @@
+/**
+ * Genre/Subject Mappings for BookHive
+ *
+ * NOTE: Google Books API uses "subject" parameter, not "genre"
+ * This file maps user-friendly genre names to Google Books subjects
+ *
+ * @module constants/genres
+ *
+ * TODO for team:
+ * - Add more genres as needed
+ * - Update icons/colors when design system is ready
+ * - Backend team: Use these same values for consistency
+ */
