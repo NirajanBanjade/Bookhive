4683f39a9f263455606fcdb3f8b11461d799b79e
Prashant Panta <uyw13@txstate.edu>
2025-10-23 22:38:06 -0500

    KAN-84 added genre routes endpoint

--- bookhive/backend/routes/booksRoutes.js
+++ bookhive/backend/routes/booksRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router();
 const {
   searchBooks,
   getTrendingBooks,
+  getBooksByGenre,
 } = require("../controllers/bookController");
 const { validateQuery } = require('../utils/validateQuery');
 
 router.get('/search', validateQuery(), searchBooks);
 router.get("/trending", getTrendingBooks);
+router.get("/genre/:subject", getBooksByGenre);
 
 module.exports = router;
