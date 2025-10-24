d0697944e3d67bd57f773b35fd29a9cdc7f31af5
lav115 <lav115@txstate.edu>
2025-10-24 14:52:30 -0500

    KAN-63 added endpoint to fetch single book

--- bookhive/backend/routes/booksRoutes.js
+++ bookhive/backend/routes/booksRoutes.js
@@ -1,15 +1,16 @@
-// routes/booksRoutes.js
 const express = require('express');
 const router = express.Router();
 const {
   searchBooks,
   getTrendingBooks,
   getBooksByGenre,
+  getBookById, 
 } = require("../controllers/bookController");
 const { validateQuery } = require('../utils/validateQuery');
 
 router.get('/search', validateQuery(), searchBooks);
 router.get("/trending", getTrendingBooks);
 router.get("/genre/:subject", getBooksByGenre);
+router.get("/:googleBookId", getBookById); 
 
 module.exports = router;
